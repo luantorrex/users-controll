bc126f681764875723e4b7fb2ba384e272944b6e
Luan Torres <luantorres360@gmail.com>
2020-08-06 16:23:17 -0300

    Add Lógica Inicial

--- main.py
+++ main.py
@@ -3,24 +3,50 @@ from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes=1)
 
 @app.route('/')
 def home():
+    if "fullname" in session:
+        return redirect(url_for("user"))
     return render_template("home.html")
-    
-@app.route('/user/')
-def user():
-    return render_template("user.html")
 
 @app.route('/signup/', methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
+        session.permanent = True
         session["fullname"] = request.form["fullname"]
         session["email"] = request.form["email"]
         session["password"] = request.form["password"]
         return redirect(url_for("user"))
     if request.method == "GET":
         return render_template("signup.html")
+    
+@app.route('/user/')
+def user():
+    return render_template("user.html")
+
+@app.route('/changeemail/', methods=["GET", "POST"])
+def changeemail():
+    if request.method == "POST":
+        session["email"] = request.form["email"]
+        return redirect(url_for("user"))
+    else:
+        if "fullname" in session:
+            return render_template("changeemail.html")
+        else:
+            return render_template("home.html")
+    
+@app.route('/changepassword/', methods=["GET", "POST"])
+def changepassword():
+    if request.method == "POST":
+        session["password"] = request.form["password"]
+        return redirect(url_for("user"))
+    else:
+        if "fullname" in session:
+            return render_template("changepassword.html")
+        else:
+            return render_template("home.html")
 
 @app.route('/signout/')
 def signout():
